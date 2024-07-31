b20dd75cbb7d0b7510cc4c3273595fde8b0cd222
Muhammad Ali <alihere786@gmail.com>
2024-07-31 11:50:06 -0700

    modified:   App.js

--- App.js
+++ App.js
@@ -1,3 +1,50 @@
+// import { StyleSheet, ImageBackground } from "react-native";
+// import StartGameScreen from "./Screens/StartGameScreen";
+// import { LinearGradient } from "expo-linear-gradient";
+// import GameScreen from "./Screens/GameScreen";
+// import { useState } from "react";
+
+// export default function App() {
+
+//   const [pickedNumber, SetPickedNumber] = useState();
+
+//   function PickedNumberHandler(enteredData){
+//     SetPickedNumber(enteredData);
+//   }
+
+//   let Screen = <StartGameScreen OnValidation={PickedNumberHandler} />
+
+//     if (pickedNumber){
+//       Screen = <GameScreen/>
+//     }
+
+
+//   return (
+//     <LinearGradient
+//       colors={["#ffa600", "#facb73", "#5d9ffa", "#0b71ff"]}
+//       style={styles.rootContainer}
+//     >
+//       <ImageBackground
+//         source={require("./Images/background.png")}
+//         resizeMode="cover"
+//         style={styles.rootContainer}
+//         imageStyle={styles.backgroundStyle}
+//       >
+//       {Screen}
+//       </ImageBackground>
+//     </LinearGradient>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   rootContainer: {
+//     flex: 1,
+//   },
+//   backgroundStyle:{
+//     opacity: 0.65,
+//   }
+// });
+
 import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./Screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
@@ -5,19 +52,23 @@ import GameScreen from "./Screens/GameScreen";
 import { useState } from "react";
 
 export default function App() {
+  const [pickedNumber, setPickedNumber] = useState(null);
+  const [isGameOver, setIsGameOver] = useState(false);
 
-  const [pickedNumber, SetPickedNumber] = useState();
-
-  function PickedNumberHandler(enteredData){
-    SetPickedNumber(enteredData);
+  function pickedNumberHandler(enteredData) {
+    setPickedNumber(enteredData);
+    setIsGameOver(false);
   }
 
-  let Screen = <StartGameScreen OnValidation={PickedNumberHandler} />
+  function gameOverHandler() {
+    setIsGameOver(true);
+  }
 
-    if (pickedNumber){
-      Screen = <GameScreen/>
-    }
+  let screen = <StartGameScreen OnValidation={pickedNumberHandler} />;
 
+  if (pickedNumber && !isGameOver) {
+    screen = <GameScreen onGameOver={gameOverHandler} />;
+  }
 
   return (
     <LinearGradient
@@ -30,7 +81,7 @@ export default function App() {
         style={styles.rootContainer}
         imageStyle={styles.backgroundStyle}
       >
-      {Screen}
+        {screen}
       </ImageBackground>
     </LinearGradient>
   );
@@ -40,7 +91,7 @@ const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
   },
-  backgroundStyle:{
+  backgroundStyle: {
     opacity: 0.65,
-  }
+  },
 });
